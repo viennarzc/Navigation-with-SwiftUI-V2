1c568db670acca6af836369ac88ee4ce408ed6a1
Viennarz Curtiz <viennarzc@gmail.com>
2022-07-12 20:36:06 +0800

    Create Project

---
+++ NavigateWithSwiftUIV2/NavigateWithSwiftUIV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigateWithSwiftUIV2
+//
+//  Created by Viennarz Curtiz on 7/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigateWithSwiftUIV2/NavigateWithSwiftUIV2/NavigateWithSwiftUIV2App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigateWithSwiftUIV2App.swift
+//  NavigateWithSwiftUIV2
+//
+//  Created by Viennarz Curtiz on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigateWithSwiftUIV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
